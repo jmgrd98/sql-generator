a553f3999c762f3b8d9fe1cb356dad8577b57af1
joaodantasdev <jmgrd98@gmail.com>
2024-04-25 16:42:28 -0300

    Update MessageDisplay.tsx

--- client/src/components/MessageDisplay.tsx
+++ client/src/components/MessageDisplay.tsx
@@ -7,7 +7,7 @@ interface MessageDisplayProps {
 
 const MessageDisplay = ({ message }: MessageDisplayProps) => {
   return (
-    <div className='w-full flex rounded gap-3 bg-black/10 p-3'>
+    <div className='w-full flex rounded gap-3 bg-black/10 p-3 text-white/50'>
       <p>•</p>
       <p>{message.content}</p>
     </div>
